b04f31ecd2b6d81989fabddcb35dcf64b07184e2
ranadheerbc <121967771+ranadheerbc@users.noreply.github.com>
2023-12-12 21:33:50 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2758
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red11\green12\blue12;}
+{\*\expandedcolortbl;;\cssrgb\c4706\c5098\c5490;}
+\paperw6800\paperh13320\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 public
+\fs24  
+\fs26 class
+\fs24  
+\fs26 HelloWorld
+\fs24  \
+\{\
+    
+\fs26 public
+\fs24  
+\fs26 static
+\fs24  
+\fs26 void
+\fs24  
+\fs26 main(String args[])
+\fs24  \{\
+        System.out.println(
+\fs26 "Hello World!"
+\fs24 );\
+    \}\
+\}\
+}
